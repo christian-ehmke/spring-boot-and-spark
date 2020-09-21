fe538f9bee27fba5be827db663432b31afe83862
christian.ehmke <christian@ehmke.email>
2020-09-21 12:27:56 +0200

    Word count example with Apache Spark running on top of Spring Boot. Can be used in a live context. Start counting words by call HTTP endpoint via POST.

---
+++ src/test/java/de/ehmke/spark/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ehmke.spark.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
